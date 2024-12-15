dddf6d4f791a38dea001ceeec66da20702cd19c0
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-12-16 00:18:11 +0200

    ex 1 is done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <ctime>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
@@ -8,33 +10,45 @@ int main()
     srand(time(NULL));
 
     int SIZE;
-    int *sum_elements = new int(0);
 
     cout << "Enter the size of the array -> ";
     cin >> SIZE;
 
-    if (SIZE <= 0)
+    if (!cin || SIZE <= 0)
     {
-        cout << "Invalid array size!" << endl;
+        cout << "Invalid input or array size!" << endl;
         return 1;
     }
 
-    int *arr = new int[SIZE];
+    int *arr_1 = new int[SIZE];
+    int *arr_2 = new int[SIZE];
 
-    cout << "Your array [  ";
+    cout << "Your first array: [   ";
 
-    for (int *ptr = arr; ptr < arr + SIZE; ptr++)
+    for (int *ptr = arr_1; ptr < arr_1 + SIZE; ptr++)
     {
-        *ptr = rand() % 10;
-        *sum_elements += *ptr;
-        cout << *ptr << "  ";
+        *ptr = rand() % 20 - 10;
+        cout << *ptr << "   ";
     }
 
     cout << "]" << endl;
-    cout << "Sum elements in your array = " << *sum_elements << endl;
 
-    delete sum_elements;
-    delete [] arr;
+    cout << "\nCopying first array to the second array...\n" << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+
+
+    cout << "Your second array: [   ";
+
+    for (int *first_ptr = arr_1, *second_ptr = arr_2; second_ptr < arr_2 + SIZE; first_ptr++, second_ptr++)
+    {
+        *second_ptr = *first_ptr;
+        cout << *second_ptr << "   ";
+    }
+
+    cout << "]" << endl;
+
+    delete[] arr_1;
+    delete[] arr_2;
 
     return 0;
 }

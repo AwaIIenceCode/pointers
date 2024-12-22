d9dede71bb4df36b5e0db16eed5b14aacb8e64ff
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-12-22 12:17:07 +0200

    ex 3 is done

--- main.cpp
+++ main.cpp
@@ -1,61 +1,52 @@
 #include <iostream>
-#include <ctime>
-#include <thread>
 
 using namespace std;
-using namespace std::chrono;
+
+void reverseCopyArray(int *source, int *destination, int size)
+{
+    int *end = source + size - 1;
+
+    for (int i = 0; i < size; i++)
+    {
+        *(destination + i) = *end;
+        end--;к
+    }
+}
 
 int main()
 {
-    srand(time(NULL));
+    int size;
 
-    int SIZE;
+    cout << "Enter the size of the array -> ";
+    cin >> size;
 
-    cout << "Enter the size of your array -> ";
-    cin >> SIZE;
-
-    if (!cin || SIZE <= 0)
+    if (size <= 0)
     {
-        cout << "Invalid input or array size!" << endl;
+        cout << "Invalid size. Exiting." << endl;
         return 1;
     }
 
-    int *first_arr = new int[SIZE];
+    int* array1 = new int[size];
+    int* array2 = new int[size];
 
-    for (int *p_i = first_arr; p_i < first_arr + SIZE; p_i++)
+    cout << "Enter elements of the array -> " << endl;
+    for (int i = 0; i < size; i++)
     {
-        cout << "Enter the " << (p_i - first_arr + 1) << " element of your array -> ";
-        cin >> *p_i;
-        cout << endl;
+        cin >> *(array1 + i);
     }
 
-    cout << "Your first array [  ";
+    reverseCopyArray(array1, array2, size);
 
-    for (int *p_i = first_arr; p_i < first_arr + SIZE; p_i++)
+    cout << "Reversed array -> ";
+
+    for (int i = 0; i < size; i++)
     {
-        cout << *p_i << "  ";
+        cout << *(array2 + i) << " ";
     }
+    cout << endl;
 
-    cout << "]" << endl;
-
-    cout << "Your reversed array [  ";
-
-    for (int *p_i = first_arr + SIZE - 1; p_i >= first_arr; p_i--)
-    {
-        cout << *p_i << "  ";
-        this_thread::sleep_for(seconds(1));
-    }
-
-    cout << "]" << endl;
-
-    delete[] first_arr;
+    delete[] array1;
+    delete[] array2;
 
     return 0;
 }
-
-/*for (int *p_start = first_arr, *p_end = first_arr + SIZE - 1; p_start < p_end; p_start++, p_end--)
-{
-int temp = *p_start;
-*p_start = *p_end;
-*p_end = temp;
-}*/
